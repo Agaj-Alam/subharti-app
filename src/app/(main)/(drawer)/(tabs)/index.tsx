51f91571bbe4daa3fcfe3797caf63b83e5654cd5
Agaj Alam <agajalam283@gmail.com>
2026-02-11 12:53:23 +0530

    reanimated update

--- src/app/(main)/(drawer)/(tabs)/index.tsx
+++ src/app/(main)/(drawer)/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import SOSSlider from '@/src/components/SOSSlicer'
 
 const index = () => {
   return (
     <View>
       <Text>SUBHARTI APP HOME PAGES</Text>
+      <SOSSlider/>
     </View>
   )
 }
